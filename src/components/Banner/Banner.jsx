e253d3104c9f0e9f91870212abe8eb079b37c781
Quang - Marco <trannhanthienquang1803@gmail.com>
2024-09-11 15:51:07 +0700

    finish call API ListJobPage

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,55 +3,12 @@ import "./banner.scss";
 import { DownOutlined } from "@ant-design/icons";
 import FormSearch from "../FormSearch/FormSearch";
 import useResponsive from "../../hooks/useResponsive";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { pathDefault } from "../../common/path";
 
 const Banner = () => {
-  const listProducts = [
-    {
-      imgURL:
-        "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/programming-tech-thin.56382a2.svg",
-      name: "Programming & Tech",
-    },
-    {
-      imgURL:
-        "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design-thin.ff38893.svg",
-      name: "Graphics & Design",
-    },
-    {
-      imgURL:
-        "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/digital-marketing-thin.68edb44.svg",
-      name: "Digital Marketing",
-    },
-    {
-      imgURL:
-        "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/writing-translation-thin.fd3699b.svg",
-      name: "Writing & Translation",
-    },
-    {
-      imgURL:
-        "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/video-animation-thin.9d3f24d.svg",
-      name: "Video & Animation",
-    },
-    {
-      imgURL:
-        "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/ai-services-thin.104f389.svg",
-      name: "AI Services",
-    },
-    {
-      imgURL:
-        "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/music-audio-thin.43a9801.svg",
-      name: "Music & Audio",
-    },
-    {
-      imgURL:
-        "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/business-thin.885e68e.svg",
-      name: "Business",
-    },
-    {
-      imgURL:
-        "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/consulting-thin.d5547ff.svg",
-      name: "Consulting",
-    },
-  ];
+  const { listDetailsJobs } = useSelector((state) => state.congViecSlice);
   const listBrands = [
     {
       imgURL:
@@ -101,9 +58,13 @@ const Banner = () => {
     <section className="banner">
       <div className="container lg:px-2">
         <div className="banner_content md:rounded-2xl md:mt-10 mb-5 h-72 md:h-96 xl:h-[500px] flex flex-col justify-around items-center">
-          <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center w-[334px] md:w-[534px] lg:w-2/3 pt-5 lg:pt-28">
-            Scale your professional workforce with{" "}
-            <i className="title_highlight font-semibold">freelancers</i>
+          <h1 className="text-white text-4xl sm:text-5xl xl:text-6xl text-center w-80 sm:w-7/12 xl:w-[800px] pt-5 sm:pt-10 xl:pt-28">
+            Find the right
+            <span className="title_highlight text-green-600 text-4xl sm:text-5xl lg:text-[63px]">
+              {" "}
+              freelance{" "}
+            </span>
+            service, right away
           </h1>
           <FormSearch
             classWrapper=""
@@ -125,17 +86,18 @@ const Banner = () => {
           </div>
         </div>
         <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-9 gap-4">
-          {listProducts.slice(0, visibleProducts).map((product, index) => (
-            <div
+          {listDetailsJobs.slice(0, visibleProducts).map((product, index) => (
+            <Link
               key={index + 1}
               className="border rounded-2xl p-3 lg:p-5 shadow-lg cursor-pointer hover:bg-cyan-100 duration-300"
+              to={`${pathDefault.listJob}?idJob=${product.id}`}
             >
               <img src={product.imgURL} />
               <p className="text-sm font-semibold mt-3">{product.name}</p>
-            </div>
+            </Link>
           ))}
         </div>
-        {visibleProducts < listProducts.length && (
+        {visibleProducts < listDetailsJobs.length && (
           <div
             onClick={handleShowMore}
             className="text-center font-semibold py-2 mt-3 rounded-md cursor-pointer hover:bg-gray-100 duration-300"
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,25 +3,12 @@ import "./navbar.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { Dropdown, Space } from "antd";
 import { getListJobs } from "../../redux/congViecSlice";
+import { Link } from "react-router-dom";
+import { pathDefault } from "../../common/path";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const { listJobs } = useSelector((state) => state.congViecSlice);
-  const [isScroll, setIsScroll] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 700) {
-        setIsScroll(true);
-      } else {
-        setIsScroll(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   useEffect(() => {
     dispatch(getListJobs());
@@ -29,41 +16,48 @@ const Navbar = () => {
 
   return (
     <>
-      {isScroll && (
-        <section className="bg-white w-screen border-b fixed top-[85px] pt-3 z-10 hidden lg:block">
-          <div className="container">
-            <div className="flex justify-between gap-4">
-              {listJobs.slice(0, 9).map((parent, parentIndex) => (
-                <Dropdown
-                  key={parentIndex + 1}
-                  menu={{
-                    items: parent.dsNhomChiTietLoai.map(
-                      (child, childIndex) => ({
-                        key: `${parentIndex + 1}-${childIndex + 1}`,
-                        type: "group",
-                        label: child.tenNhom,
-                        children: child.dsChiTietLoai.map(
-                          (subchild, subchildIndex) => ({
-                            key: `${parentIndex + 1}-${
-                              childIndex + 1
-                            }-${subchildIndex}`,
-                            label: subchild.tenChiTiet,
-                          })
+      <section className="bg-white w-screen border-b fixed top-[85px] pt-3 z-10 hidden lg:block">
+        <div className="container">
+          <div className="flex justify-between gap-4">
+            {listJobs.slice(0, 9).map((parent, parentIndex) => (
+              <Dropdown
+                key={parentIndex + 1}
+                menu={{
+                  items: parent.dsNhomChiTietLoai.map((child, childIndex) => ({
+                    key: `${parentIndex + 1}-${childIndex + 1}`,
+                    type: "group",
+                    label: child.tenNhom,
+                    children: child.dsChiTietLoai.map(
+                      (subchild, subchildIndex) => ({
+                        key: `${parentIndex + 1}-${
+                          childIndex + 1
+                        }-${subchildIndex}`,
+                        label: (
+                          <Link
+                            to={`${pathDefault.listJob}?idDetails=${subchild.id}`}
+                          >
+                            {subchild.tenChiTiet}
+                          </Link>
                         ),
                       })
                     ),
-                  }}
-                  className="text-gray-600 cursor-pointer pb-2 border-b-[3px] border-white duration-300"
-                >
-                  <a onClick={(e) => e.preventDefault()}>
-                    <Space>{parent.tenLoaiCongViec}</Space>
-                  </a>
-                </Dropdown>
-              ))}
-            </div>
+                  })),
+                }}
+                className="text-gray-600 cursor-pointer pb-2 border-b-[3px] border-white duration-300"
+              >
+                <Space>
+                  <Link
+                    className="hover:text-gray-500"
+                    to={`${pathDefault.listJob}?idJob=${parent.id}`}
+                  >
+                    {parent.tenLoaiCongViec}
+                  </Link>
+                </Space>
+              </Dropdown>
+            ))}
           </div>
-        </section>
-      )}
+        </div>
+      </section>
     </>
   );
 };
