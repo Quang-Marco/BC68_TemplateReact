01bce6773e56727bf399addecfa8c1063e1d1fbd
Quang-Marco <trannhanthienquang1803@gmail.com>
2024-10-03 20:23:07 +0700

    update login facebook

---
+++ src/components/LoginFacebook.jsx
@@ -0,0 +1,67 @@
+import React, { useContext } from "react";
+import FacebookLogin from "react-facebook-login";
+import { useNavigate } from "react-router-dom";
+import { authService } from "../services/auth.service";
+import { nguoiDungService } from "../services/nguoiDung.service";
+import { setLocalStorage } from "../utils/utils";
+import { NotificationContext } from "../App";
+import { pathDefault } from "../common/path";
+
+const LoginFacebook = () => {
+  const navigate = useNavigate();
+  const { handleNotification } = useContext(NotificationContext);
+
+  const responseFacebook = async (response) => {
+    const { name, email } = response;
+    const password = "Admin123@";
+    const avatarUrl = response.picture.data.url;
+    localStorage.setItem("avatar", avatarUrl);
+    const userFacebook = { name, email, password };
+
+    try {
+      await authService.signUp(userFacebook);
+    } catch (err) {
+      if (err.response?.data?.content?.includes("Email đã tồn tại")) {
+        console.log("Email đã tồn tại, tiếp tục đăng nhập...");
+      } else {
+        console.log(err);
+        handleNotification("Registration failed", "error");
+        return;
+      }
+    }
+
+    try {
+      const signInResponse = await authService.signIn({ email, password });
+      const user = signInResponse.data.content;
+      setLocalStorage("userFacebook", user);
+      handleNotification("Login successful!", "success");
+      navigate(pathDefault.homePage);
+
+      // Fetch avatar and upload it
+      const avatarResponse = await fetch(avatarUrl);
+      const blob = await avatarResponse.blob();
+
+      const formData = new FormData();
+      formData.append("formFile", blob, "avatar.jpg");
+
+      // Gọi API upload avatar
+      await nguoiDungService.uploadAvatar(user.token, formData);
+      console.log("Avatar uploaded successfully!");
+    } catch (err) {
+      console.log("Error uploading avatar:", err);
+      handleNotification(err.response.data.message || "Login failed", "error");
+    }
+  };
+  return (
+    <div>
+      <FacebookLogin
+        appId="836842291948348"
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={responseFacebook}
+      />
+    </div>
+  );
+};
+
+export default LoginFacebook;
